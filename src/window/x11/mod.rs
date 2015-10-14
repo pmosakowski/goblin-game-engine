4bceec73450d2c89fb8425350f5c47bf1f522cf7
pmosakowski <pmosakowski@users.noreply.github.com>
2015-10-14 12:54:12 +0100

    Added Xlib bitmap display capabilities

--- src/window/x11/mod.rs
+++ src/window/x11/mod.rs
@@ -11,6 +11,7 @@ pub struct X11Window<'a> {
     display: &'a mut xlib::Display,
     screen: i32,
     window: xlib::Window,
+    visual: &'a mut xlib::Visual,
     gc: xlib::GC,
 }
 
@@ -22,6 +23,7 @@ impl<'a> X11Window<'a> {
                 return Err(String::from("Can't open display"))
             }
             let scr = xlib::XDefaultScreen(disp);
+            let vis = xlib::XDefaultVisual(disp, scr);
             let root_win = xlib::XRootWindow(disp,scr);
             let black = xlib::XBlackPixel(disp, scr);
             let white = xlib::XWhitePixel(disp, scr);
@@ -48,12 +50,12 @@ impl<'a> X11Window<'a> {
             let mut gcval: xlib::XGCValues = zeroed();
             let gc = xlib::XCreateGC(disp, win, 0, &mut gcval);
 
-
             Ok(X11Window {
                 width: width,
                 height: height,
                 display: &mut *disp,
                 screen: scr,
+                visual: &mut *vis,
                 window: win,
                 gc: gc,
             })
@@ -62,10 +64,20 @@ impl<'a> X11Window<'a> {
 
     pub fn update(&mut self) {
         unsafe {
-            let black = xlib::XBlackPixel(self.display, self.screen);
-            xlib::XSetForeground(self.display, self.gc, black);
-            xlib::XDrawLine(self.display, self.window, self.gc, 10, 10,190,190); //from-to
-            xlib::XDrawLine(self.display, self.window, self.gc, 10,190,190, 10);
+            let mut buffer = Image::new(self.display, self.visual, 640, 480);
+
+            for x in 0..640 {
+                for y in 0..480 {
+                    let pad = 00;
+                    let red = (x % 256);
+                    let green = (y % 256);
+                    let blue = 256 - (x % 256);
+                    let color = (pad << 24) + (red << 16) + (green << 8) + blue;
+                    xlib::XPutPixel(buffer.ximage, x, y, color as u64);
+                };
+            };
+
+            self.blit(&mut buffer);
 
             let mouse_events = xlib::ButtonPressMask | xlib::ButtonReleaseMask;
             xlib::XSelectInput(self.display, self.window, mouse_events);
@@ -80,10 +92,51 @@ impl<'a> X11Window<'a> {
         }
     }
 
+    pub fn blit(&mut self, img: &mut Image) {
+        unsafe {
+            xlib::XPutImage(self.display,
+                            self.window,
+                            self.gc,
+                            img.ximage,
+                            0,0,
+                            0,0,
+                            img.width,
+                            img.height,)
+        };
+    }
+
     pub fn close(&mut self) {
         unsafe {
             xlib::XDestroyWindow(self.display, self.window);
             xlib::XCloseDisplay(self.display);
+        }
+    }
+}
+
+pub struct Image<'a> {
+    ximage: &'a mut xlib::XImage,
+    data: Vec<i8>,
+    width: u32,
+    height: u32,
+}
+
+impl<'a> Image<'a> {
+    pub fn new(disp: &mut xlib::Display, vis: &mut xlib::Visual, width: u32, height: u32) -> Image<'a> {
+        let bitmap_pad: i32 = 32;
+        let bytes_per_line: i32 = (width as i32)*bitmap_pad/4;
+        let mut data: Vec<i8> = vec![0; (width*height*4) as usize];
+        let mut ximg_ref = unsafe {
+            let ximg_ptr = xlib::XCreateImage(disp, vis, 24, xlib::ZPixmap, 0,
+                                              data.as_mut_ptr(), width, height, bitmap_pad, bytes_per_line);
+            xlib::XInitImage(ximg_ptr);
+            &mut *ximg_ptr
+        };
+
+        Image {
+            ximage: ximg_ref,
+            data: data,
+            width: width,
+            height: height,
         }
     }
 }
